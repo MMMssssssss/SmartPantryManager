76f6deeae7d5cad7d4510939849061d20326d0a9
MMMssssssss <157210623+MMMssssssss@users.noreply.github.com>
2026-09-08 21:14:43 +0200

    Create pantry list screen

---
+++ app/src/main/java/com/marushka/smartpantrymanager/activities/PantryActivity.java
@@ -0,0 +1,21 @@
+package com.marushka.smartpantrymanager.activities;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.marushka.smartpantrymanager.R;
+
+public class PantryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
