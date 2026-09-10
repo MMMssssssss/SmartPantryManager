4a1a683e9bde8b12c8fd95b53d4221dca7f80c40
MMMssssssss <157210623+MMMssssssss@users.noreply.github.com>
2026-09-10 15:19:21 +0200

    Add RecyclerView to Pantry List screen

--- app/src/main/java/com/marushka/smartpantrymanager/MainActivity.java
+++ app/src/main/java/com/marushka/smartpantrymanager/MainActivity.java
@@ -1,6 +1,10 @@
 package com.marushka.smartpantrymanager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +12,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.marushka.smartpantrymanager.activities.PantryActivity;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView appTitle;
+    Button getStarted;
+    ImageView pantryImage;
+    TextView description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        getStarted = findViewById(R.id.getStarted);
+        appTitle = findViewById(R.id.appTitle);
+        pantryImage = findViewById(R.id.pantryImage);
+        description = findViewById(R.id.description);
+
+        getStarted.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, PantryActivity.class);
+            startActivity(intent);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/marushka/smartpantrymanager/activities/PantryActivity.java
+++ app/src/main/java/com/marushka/smartpantrymanager/activities/PantryActivity.java
@@ -12,10 +12,8 @@ public class PantryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
-
-
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_pantry);
+
     }
 }
